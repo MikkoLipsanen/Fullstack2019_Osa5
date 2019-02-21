06eaa5bc1fddfba9600ed2d05b1f60bf48d6f11d
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-02-22 00:06:50 +0200

    Tehtävät 5.3. ja 5.4.

--- bloglist_frontend/src/App.js
+++ bloglist_frontend/src/App.js
@@ -3,13 +3,40 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login' 
 
-console.log(window.localStorage.getItem('loggedBlogappUser'))
+const Notification = ({ notification }) => {
+  if (notification.message === null) {
+    return null
+  }
+
+  const style = {
+    color: notification.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+
+  return (
+    <div style={style}>
+      {notification.message}
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [newLikes, setNewLikes] = useState('')
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null) 
+  const [notification, setNotification] = useState({
+    message: null
+  })
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -22,12 +49,46 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
+  const notify = (message, type='success') => {
+    setNotification({ message, type })
+    setTimeout(() => setNotification({ message: null }), 10000)
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    notify(`${user.name} logged out`)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+      const blogObject = {
+        title: newTitle,
+        author: newAuthor,
+        url: newUrl,
+        likes: newLikes
+      }
+
+      blogService
+        .create(blogObject).then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setNewTitle('')
+          setNewAuthor('')
+          setNewUrl('')
+          setNewLikes('')
+          notify(`A new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
+        })
+        .catch(error => {
+          notify(`Blog creation failed`, 'error')        
+        })
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
-
+    try {
       const user = await loginService.login({
         username, password,
       })
@@ -36,15 +97,27 @@ const App = () => {
         'loggedBlogappUser', JSON.stringify(user)
       ) 
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
+      notify(`${user.name} logged in`)
+    } catch (exception) {
+      notify(`Wrong username or password`, 'error')
+    }
   }
 
-  if (user === null) {
-    return (
-      <div>
+  const rows = () => blogs.map(blog =>
+    <Blog 
+      key={blog.id} 
+      blog={blog}
+    />
+  )
+
+  const loginForm = () => (
+    <div>
         <h2>Log in to application</h2>
+        <Notification notification={notification} />
         <form onSubmit={handleLogin}>
           <div>
             käyttäjätunnus
@@ -67,22 +140,66 @@ const App = () => {
           <button type="submit">kirjaudu</button>
         </form>
       </div>
-    )
-  }
+  )
 
-  return (
+  const blogForm = () => (
     <div>
-      <p>{user.name} logged in</p>
-      <button onClick = 
-        {() => window.localStorage.removeItem('loggedBlogappUser')}>logout
-      </button>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <form onSubmit={addBlog}>
+        <div>
+          title: 
+            <input 
+            type="text"
+            value={newTitle} 
+            onChange={({ target }) => setNewTitle(target.value)}
+          />
+        </div>
+        <div> 
+          author: 
+            <input 
+            type="text"
+            value={newAuthor}
+            onChange={({ target }) => setNewAuthor(target.value)}
+          />
+        </div>
+        <div> 
+          url: 
+            <input 
+            type="text"
+            value={newUrl} 
+            onChange={({ target }) => setNewUrl(target.value)}
+          />
+        </div>
+        <div> 
+          likes: 
+            <input 
+            type="text"
+            value={newLikes} 
+            onChange={({ target }) => setNewLikes(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>  
     </div>
   )
 
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <h2>blogs</h2>
+          <Notification notification={notification} />
+          <p>{user.name} logged in</p>
+          <button onClick = {handleLogout}>logout </button>
+          <h2>create new</h2>
+            {blogForm()}
+          <ul>
+            {rows()}
+          </ul> 
+        </div>
+      }
+    </div>
+  )
 }
 
 export default App
